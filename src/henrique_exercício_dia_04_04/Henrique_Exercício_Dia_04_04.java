df55ebf27e9a9a65b1e959503291165cbd850235
henri <henriquerv12@outlook.com>
2018-04-06 20:54:24 -0300

    TESTANDOOO

---
+++ src/henrique_exercício_dia_04_04/Henrique_Exercício_Dia_04_04.java
@@ -0,0 +1,53 @@
+package henrique_exercício_dia_04_04;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Henrique Santos Ludovino
+ */
+public class Henrique_Exercício_Dia_04_04 {
+
+    public static void main(String[] args) {
+        try {
+            Scanner ler = new Scanner(System.in);
+            Produtos prod = new Produtos();
+            prod.setCodigo(00001);
+            prod.setCodigo2(00002);
+            prod.setCodigo3(00003);
+            prod.setNome("Bolacha");
+            prod.setNome2("Coca-Cola");
+            prod.setNome3("Sprite");
+            prod.setEstoque(444);
+            prod.setEstoque2(443);
+            prod.setEstoque3(443);
+            prod.setFabricante("Nestlé");
+            prod.setFabricante2("Coca-Cola Brasil");
+            prod.setFabricante3("Coca-Cola Brasil");
+            if (prod.getEstoque() > prod.getEstoque2() && prod.getEstoque() > prod.getEstoque3()) {
+                System.out.println("Produto com Maior Estoque:");
+                System.out.println("Código: " + prod.getCodigo());
+                System.out.println("Produto: " + prod.getNome());
+                System.out.println("Quantidade em estoque: " + prod.getEstoque());
+                System.out.println("Fabricante: "+prod.getFabricante());
+            }
+            if (prod.getEstoque2() > prod.getEstoque() && prod.getEstoque2() > prod.getEstoque3()) {
+                System.out.println("Produto com Maior Estoque:");
+                System.out.println("Código: " + prod.getCodigo2());
+                System.out.println("Produto: " +prod.getNome2());
+                System.out.println("Quantidade em estoque: " +prod.getEstoque2());
+                System.out.println("Fabricante: "+ prod.getFabricante2());
+            }
+            if (prod.getEstoque3() > prod.getEstoque() && prod.getEstoque3() > prod.getEstoque2()) {
+                System.out.println("Produto com Maior Estoque:");
+                System.out.println("Código: " + prod.getCodigo3());
+                System.out.println("Produto: "+prod.getNome3());
+                System.out.println("Quantidade em estoque: " +prod.getEstoque3());
+                System.out.println("Fabricante: "+prod.getFabricante3());
+            }
+        } catch (Exception vetor) {
+            System.out.println("ERRO: Valor informado não é de um tipo válido ");
+        }
+    }
+
+}
---
+++ src/henrique_exercício_dia_04_04/Produtos.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package henrique_exercício_dia_04_04;
+
+/**
+ *
+ * @author henri
+ */
+public class Produtos {
+    private String nome, nome2, nome3,fabricante, fabricante2,fabricante3;
+    private int codigo, codigo2, codigo3,estoque, estoque2, estoque3;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+     public String getNome2() {
+        return nome2;
+    }
+
+    public void setNome2(String nome2) {
+        this.nome2 = nome2;
+    }
+    
+     public String getNome3() {
+        return nome3;
+    }
+
+    public void setNome3(String nome3) {
+        this.nome3 = nome3;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+    
+    public String getFabricante2() {
+        return fabricante2;
+    }
+
+    public void setFabricante2(String fabricante2) {
+        this.fabricante2 = fabricante2;
+    }
+    
+    public String getFabricante3() {
+        return fabricante3;
+    }
+
+    public void setFabricante3(String fabricante3) {
+        this.fabricante3 = fabricante3;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    public int getCodigo2() {
+        return codigo2;
+    }
+
+    public void setCodigo2(int codigo2) {
+        this.codigo2 = codigo2;
+    }
+    
+    public int getCodigo3() {
+        return codigo3;
+    }
+
+    public void setCodigo3(int codigo3) {
+        this.codigo3 = codigo3;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+    
+    public int getEstoque2() {
+        return estoque2;
+    }
+
+    public void setEstoque2(int estoque2) {
+        this.estoque2 = estoque2;
+    }
+    
+    public int getEstoque3() {
+        return estoque3;
+    }
+
+    public void setEstoque3(int estoque3) {
+        this.estoque3 = estoque3;
+    }
+    
+    
+}
